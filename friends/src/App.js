adb48009bb86ee07e5c22a1a6c68a023dc60bd72
coriagogo <pkcparis@gmail.com>
2019-05-16 16:47:20 -0400

    privateroute and login created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import PrivateRoute from './PrivateRoute';
+import Login from './components/Login';
 
 import './App.css';
 
